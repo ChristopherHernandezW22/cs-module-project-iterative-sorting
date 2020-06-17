f287e367f62c5dfdd413d1be8242769914e07733
Christopher Hernandez <cerberean661@gmail.com>
2020-06-16 22:32:23 -0700

    updated changes

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -14,8 +14,9 @@ print(linear_search([3,5,2,1,4,8,9,7,6], 6))
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
-
     # Your code here
 
 
     return -1  # not found
+
+print(binary_search([3,5,2,1,4,8,9,7,6], 6))
